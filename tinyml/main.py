4b3e8748150f35adc0b5bd54e66f77b27339a964
Yassien Shaalan <yassien@growingdata.com.au>
2025-09-22 20:52:29 +1000

    fixed flag

--- tinyml/main.py
+++ tinyml/main.py
@@ -111,13 +111,15 @@ def _mitdb_gcs_wrapper(batch_size=64, length=1800, binary=True, **_):
 register_dataset('apnea_ecg', _apnea_gcs_wrapper)
 
 # Flip these to True if you want them in this run
+'''
 REGISTER_PTB = True
 REGISTER_MIT = True
 if REGISTER_PTB:
     register_dataset('ptbxl', _ptbxl_gcs_wrapper)
 if REGISTER_MIT:
     register_dataset('mitdb', _mitdb_gcs_wrapper)
-
+'''
+import argparse
 # -------------------- Config --------------------
 cfg = ExpCfg(
     epochs=8,
